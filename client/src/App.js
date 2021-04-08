79e6c005babac39cf3ac6641ff8a37ab494deaa0
jackrc7 <cardin.j@husky.neu.edu>
2021-04-08 18:23:49 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from "./components/Home";
 
 
+
+
 class App extends Component {
   render() {
     return (
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import Map from "./Map.js"
 
 class Home extends Component {
 
+	// Now export the above Map component into another component by passing the props values
 	render() {
-		return(
-			<div>
-				Home
-			</div>
-		);
+	  return(
+		  <Map
+	   google={this.props.google}
+	   center={{lat: 18.5204, lng: 73.8567}}
+	   height='300px'
+	   zoom={15}
+	  />
+		)
 	}
 }
 
